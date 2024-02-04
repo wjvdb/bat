e25198dffc3f2f18674fdd5063384b939421dde4
wimjan <wimjan99@gmail.com>
2024-02-04 22:12:10 +0100

    Update setup.bat
    cleanuop of the fockup also now show file extensions plz hackscribe to my yoputub that i dont use

--- setup.bat
+++ setup.bat
@@ -17,102 +17,45 @@ IF %ERRORLEVEL% EQU 0 (
 
 setlocal enabledelayedexpansion
 
-set /a count=0
-set /a total=18
-
 echo Setting registry values...
 
 REM Add registry values and display progress
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve >nul
 echo Setting registry value: HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "BingSearchEnabled" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Search\BingSearchEnabled 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarAl" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarAl 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarGlomLevel" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarGlomLevel 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowTaskViewButton" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ShowTaskViewButton 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Search\SearchboxTaskbarMode 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "OneDrive" /f >nul
 echo Deleting registry value: HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\OneDrive 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "EnableXamlStartMenu" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\EnableXamlStartMenu 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "EnableXamlStartMenuExperience" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\EnableXamlStartMenuExperience 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_PowerButtonAction" /t REG_DWORD /d 2 /f >nul
 echo Setting registry value: HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Start_PowerButtonAction 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowDomainPINLogon" /t REG_DWORD /d 1 /f >nul
 echo Setting registry value: HKLM\SOFTWARE\Policies\Microsoft\Windows\System\AllowDomainPINLogon 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowDomainPINLogonFallback" /t REG_DWORD /d 1 /f >nul
 echo Setting registry value: HKLM\SOFTWARE\Policies\Microsoft\Windows\System\AllowDomainPINLogonFallback 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowDomainPINLogonWithBiometrics" /t REG_DWORD /d 1 /f >nul
 echo Setting registry value: HKLM\SOFTWARE\Policies\Microsoft\Windows\System\AllowDomainPINLogonWithBiometrics 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowDomainPINLogonWithFingerprint" /t REG_DWORD /d 1 /f >nul
 echo Setting registry value: HKLM\SOFTWARE\Policies\Microsoft\Windows\System\AllowDomainPINLogonWithFingerprint 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowDomainPINLogonWithPIN" /t REG_DWORD /d 1 /f >nul
 echo Setting registry value: HKLM\SOFTWARE\Policies\Microsoft\Windows\System\AllowDomainPINLogonWithPIN 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Control Panel\Colors" /v "Background" /t REG_SZ /d "0 0 0" /f >nul
 echo Setting registry value: HKCU\Control Panel\Colors\Background 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize\AppsUseLightTheme 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize\SystemUsesLightTheme 
-set /a count+=1
-echo Progress: !count! out of !total! [!count!%%]
-
 reg add "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v "TabPreloader" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main\TabPreloader
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge" /v "BackgroundAccessStatus" /t REG_DWORD /d 0 /f >nul
@@ -135,7 +78,13 @@ reg add "HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotRepo
 echo Setting registry value: HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting\value
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AppHost" /v "EnableWebContentEvaluation" /t REG_DWORD /d 0 /f >nul
 echo Setting registry value: HKCU\Software\Microsoft\Windows\CurrentVersion\AppHost\EnableWebContentEvaluation
+set key=HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced
 
+reg add "%key%" /f /v HideFileExt               /t REG_DWORD /d 0
+reg add "%key%" /f /v ShowCortanaButton         /t REG_DWORD /d 0
+reg add "%key%" /f /v ShowTaskViewButton        /t REG_DWORD /d 0
+reg add "%key%" /f /v StoreAppsOnTaskbar        /t REG_DWORD /d 0
+reg add "%key%" /f /v TaskbarAnimations         /t REG_DWORD /d 0
 
 echo.
 echo All registry values have been set successfully.
